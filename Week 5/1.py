0798736bc53003c88c598352c39a9620e29c0472
SaraFelman <yrakotam@gmail.com>
2025-10-06 23:16:08 +0300

    Чет пытались умное написать по задачи 1

--- Week 5/1.py
+++ Week 5/1.py
@@ -5,16 +5,12 @@ with open(" BindingEnergy.txt", encoding='utf-8') as r_file:
     # Создаем объект reader, указываем символ-разделитель ","
     file_reader = csv.reader(r_file, delimiter = ",")
     # Счетчик для подсчета количества строк и вывода заголовков столбцов
-    count = 0
-    sum_kvad_otkl = []
-    
-    srednee = 
-    n = 6
+    count = []
+   
     # Считывание данных из CSV файла
     for row in file_reader:
-        kvad_otkl = ( ( (nabludenie - srednee)**2) / n) ** 0.5
-        if kvad_otkl > count:
-            count = kvad_otkl
+        sum_kvad_otkl = (row["m1"] - row["control"])**2 + (row["m2"] - row["control"])**2 + (row["m3"] - row["control"])**2 + (row["m4"] - row["control"])**2 + (row["m5"] - row["control"])**2 + (row["m6"] - row["control"])**2
+        
 
             # Вывод строки, содержащей заголовки для столбцов
             print(f'Файл содержит столбцы: {", ".join(row)}')
